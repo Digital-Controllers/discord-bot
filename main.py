28523f2adcdcbcc66f4a0fddfa267a82899510c1
QuantifyGG <75543442+quantifygg@users.noreply.github.com>
2023-05-03 23:56:12 +0100

    feat: introductory strip creation

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from discord import app_commands
 from discord.app_commands import Choice
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+import random
 
 sys.path.insert(0, "venv/Lib/site-packages")
 
@@ -63,6 +65,12 @@ bot = commands.Bot(command_prefix='t?', intents=intents)
 
 print(f"Started at {str(datetime.now(timezone.utc))[:-16]}")
 
+JETS = ["F16", "F18", "F15", "F35", "F22", "A10", "F14", "MIR2"]
+HOLDING_POINTS = ["A", "B", "C", "D"]
+AERODROMES = ["UG5X", "UG24", "UGKO", "UGKS", "URKA", "URKN", "URMM", "URSS"]
+RUNWAYS = ["22", "04"]
+DEPARTURES = ["GAM1D", "PAL1D", "ARN1D", "TIB1D", "SOR1D", "RUD1D", "AGI1D", "DIB1D", "TUN1D", "NAL1D"]
+
 
 # usage:
 # @bot.command()
@@ -87,8 +95,31 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    await bot.get_channel(1099805424934469652).send(f"Welcome to Digital Controllers, {member.name}!")
-
+    time = str(datetime.now())
+    strip_text = {
+        "slot": f"{time[11:16]}",
+        "squawk": f"{random.randint(0, 6)}{random.randint(0, 7)}{random.randint(0, 7)}{random.randint(0, 7)}",
+        "callsign": f"{member.name.upper()[:4]}{random.randint(0, 9)}{random.randint(0, 9)}",
+        "aircraft": f"{random.choice(JETS)}",
+        "hold": f"{random.choice(HOLDING_POINTS)}",
+        "aerodrome": f"{random.choice(AERODROMES)}",
+        "runway": f"{random.choice(RUNWAYS)}",
+        "departure": f"{random.choice(DEPARTURES)}"
+    }
+    strip = Image.open("strip_blank.png")
+    font = ImageFont.truetype("consolas.ttf", 40)
+    font_large = ImageFont.truetype("consolas.ttf", 70)
+    d = ImageDraw.Draw(strip)
+    d.text((67, 101), strip_text["slot"], font=font, fill=(0, 0, 0), anchor="mm")
+    d.text((305, 101), strip_text["callsign"], font=font_large, fill=(0, 0, 0), anchor="lm")
+    d.text((914, 101), strip_text["hold"], font=font_large, fill=(0, 0, 0), anchor="mm")
+    d.text((1339, 150), strip_text["aerodrome"], font=font, fill=(0, 0, 0), anchor="mm")
+    d.text((1632, 101), strip_text["runway"], font=font_large, fill=(0, 0, 0), anchor="mm")
+    d.text((1803, 101), strip_text["departure"], font=font_large, fill=(0, 0, 0), anchor="mm")
+    d.text((646, 150), strip_text["squawk"], font=font, fill=(0, 0, 0), anchor="lm")
+    d.text((646, 57), "M/" + strip_text["aircraft"], font=font, fill=(0, 0, 0), anchor="lm")
+    strip.save(fp="strip.png")
+    await bot.get_channel(1099805424934469652).send(f"Welcome to Digital Controllers, {member.name}!", file=discord.File("strip.png"))
 
 @bot.command()
 @check_is_owner()
