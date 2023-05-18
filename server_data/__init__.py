86e363fdc3b2065f0b283402910e54c5b29ba665
JustOneOther <65628260+JustOneOther@users.noreply.github.com>
2023-05-18 22:12:22 +0200

    fix(server_data): make imports relative

--- server_data/__init__.py
+++ server_data/__init__.py
@@ -1,16 +1,16 @@
-from hoggit import get_hoggit
-from limakilo import get_lk
+from .hoggit import get_hoggit as _get_hoggit
+from .limakilo import get_lk as _get_lk
 
 
 # Easy interface for variable-like gets
 def __getattr__(name):
 	if name == 'gaw':
-		return get_hoggit('gaw')
+		return _get_hoggit('gaw')
 	elif name == 'pgaw':
-		return get_hoggit('pgaw')
+		return _get_hoggit('pgaw')
 	elif name == 'lkeu':
-		return get_lk('eu')
+		return _get_lk('eu')
 	elif name == 'lkna':
-		return get_lk('na')
+		return _get_lk('na')
 	else:
 		raise AttributeError(f'Unknown attribute "{name}" in module "server_data"')
