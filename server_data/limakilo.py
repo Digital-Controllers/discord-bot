704c666d858fb92bd28326c0da0002ec9d2a43e1
JustOneOther <65628260+JustOneOther@users.noreply.github.com>
2023-05-19 13:48:23 +0200

    fix(server_data): Null response throwing TypeError

--- server_data/limakilo.py
+++ server_data/limakilo.py
@@ -9,6 +9,9 @@ def get_lk(server: Literal['eu', 'na']) -> dict:
 		response = pipe.read().decode('utf-8')
 	data_dict = loads(response)
 
+	if data_dict is None:
+		return {'exception': 'Server offline'}
+
 	seconds_to_restart = timedelta(seconds=int(data_dict['restartPeriod']) - int(data_dict['modelTime']))
 
 	return {'player_count': f"{int(data_dict['players']['current']) - 1} player(s) online",
