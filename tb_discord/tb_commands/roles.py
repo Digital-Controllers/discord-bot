b80c4beef5f992b802daeb735d351040a716ee6b
JustOneOther <65628260+JustOneOther@users.noreply.github.com>
2023-06-11 16:24:22 +0200

    feat: role button implementation

---
+++ tb_discord/tb_commands/roles.py
@@ -0,0 +1,64 @@
+from datetime import datetime, timedelta
+from discord.ui import Button, RoleSelect, View
+from discord import app_commands, Message, Interaction, Role, TextChannel, utils
+
+
+__all__ = ['command_list']
+
+
+class RoleChoiceView(View):
+	def __init__(self, message: Message):
+		super().__init__(timeout=120)
+		self.message = message
+		self.roles = TBRolesSelect()
+		self.add_item(self.roles)
+
+
+class TBRolesSelect(RoleSelect):
+	def __init__(self):
+		super().__init__(min_values=1, max_values=10)
+
+	async def callback(self, interaction: Interaction):
+		self.view.stop()
+
+
+class RolesView(View):
+	def __init__(self, roles: list[Role], *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for role in roles:
+			self.add_item(RoleButton(role))
+
+
+class RoleButton(Button):
+	def __init__(self, role: Role, **kwargs):
+		super().__init__(label=role.name, **kwargs)
+		self.role = role
+
+	async def callback(self, interaction: Interaction):
+		if self.role in interaction.user.roles:
+			await interaction.user.remove_roles(self.role)
+		else:
+			await interaction.user.add_roles(self.role)
+		await interaction.response.defer()
+
+
+@app_commands.command()
+async def create_reaction_role(interaction: Interaction, channel: TextChannel, message_id: int = None):
+	if message_id:
+		message: Message = await channel.fetch_message(message_id)
+	else:
+		message = await utils.get(
+			channel.history(limit=50, after=datetime.now() - timedelta(hours=1)), author=interaction.user)
+		if not message:
+			await interaction.response.send_message(f'Your message in {channel.name} could not be found')
+			return
+
+	choice_view = RoleChoiceView(message)
+	await interaction.response.send_message(view=choice_view, ephemeral=True)
+	await choice_view.wait()
+	out_message = await channel.send(content=message.content, embeds=message.embeds, view=RolesView(choice_view.roles.values))
+	await message.delete()
+	await interaction.delete_original_response()
+
+
+command_list = [create_reaction_role]
