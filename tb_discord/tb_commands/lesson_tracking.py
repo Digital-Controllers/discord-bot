fb06aaaac3eeaaf73769b5117a9ccc7e76262866
JustOneOther <65628260+JustOneOther@users.noreply.github.com>
2024-01-06 19:16:03 -0600

    fix: handle multiple attempted registries

--- tb_discord/tb_commands/lesson_tracking.py
+++ tb_discord/tb_commands/lesson_tracking.py
@@ -8,8 +8,12 @@ __all__ = ["command_list"]
 
 @app_commands.command()
 async def register(inter: Interaction):
-	sql_op("INSERT INTO students VALUES (%s, default, default, default)", (inter.user.id,))
-	await inter.response.send_message("Registered successfully", ephemeral=True)
+	if sql_op("SELECT COUNT(1) FROM students WHERE uid = %s", (inter.user.id,)) == (0,):
+		sql_op("INSERT INTO students VALUES (%s, default, default, default)", (inter.user.id,))
+		await inter.response.send_message("Registered successfully", ephemeral=True)
+	else:
+		await inter.response.send_message("You have already registered with Towerbot. To unregister, please contact a DC Staff member",
+										  ephemeral=True)
 
 
 @app_commands.command()
@@ -23,7 +27,7 @@ async def request_training(inter: Interaction, branch: int, lesson_num: int):
 
 	database = sql_op("SELECT requests FROM students where uid = %s", (inter.user.id,))
 	if database is None:
-		await inter.response.send_message("You have not registered with towerbot, please use /register", ephemeral=True)
+		await inter.response.send_message("You have not registered with Towerbot, please use /register", ephemeral=True)
 		return
 
 	# Handle request in student database
@@ -41,10 +45,13 @@ async def request_training(inter: Interaction, branch: int, lesson_num: int):
 	request_counts = [(request_count_bytes[i] << 8) + request_count_bytes[i + 1] for i in range(0, len(request_count_bytes), 2)]
 	request_counts[int.from_bytes(to_request, "big", signed=False)] += 1
 
+	# Request counts are stored in a bytestring indexed by the request number as described above. Request counts themselves are
+	# a two byte big-endian unsigned integer. Ex. ACAD-02 is stored in bytes 2 & 3
 	request_count_bytes = b"".join(map(lambda x: x.to_bytes(2, "big", signed=False), request_counts))
 
 	await inter.response.send_message(f"Request processed", ephemeral=True)
 
+	# Send sql updates to database
 	sql_op("UPDATE server_data SET data = %s WHERE id = 0", (request_count_bytes,))
 	sql_op("UPDATE students SET requests = %s WHERE uid = %s", (requests, inter.user.id))
 
